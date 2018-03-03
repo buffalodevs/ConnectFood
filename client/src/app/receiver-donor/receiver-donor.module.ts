9fd397388ea3483359c44edc0c56a519a40accb5
Mark Nemmer <marknemm@buffalo.edu>
2018-03-03 15:10:03 -0500

    Removed debug code. Removed animations.

--- client/src/app/receiver-donor/receiver-donor.module.ts
+++ client/src/app/receiver-donor/receiver-donor.module.ts
@@ -28,6 +28,7 @@ import { FoodListingCartComponent } from './food-listing-cart/food-listing-cart.
 import { RoutePreprocessService } from '../common-util/services/route-preprocess.service';
 import { FoodTypesService } from '../domain/food-types/food-types.service';
 import { ManageFoodListingService } from './food-listings/food-listing-services/manage-food-listing.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 
 const receiverDonorRoutes: Routes = [
@@ -76,6 +77,7 @@ const receiverDonorRoutes: Routes = [
         SlickExpansionPanelModule,
         SlickFilteredListModule,
         SlickImgManagerModule,
+        NoopAnimationsModule,
         CommonFoodListingDomainsModule
     ],
     providers: [
